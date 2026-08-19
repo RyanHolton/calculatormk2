ed4c825c4d7693af79dc2d41b9291556780d2177
ryan_holton <ryan.m.holton@gmail.com>
2026-08-19 01:24:41 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::io;
+use num_integer::gcd;
+
+fn main(){
+    loop {
+        calculation();
+    }
+}
+
+fn calculation() {
+    let mut first_number: String = String::new();
+    
+    println!("what is your first number?");
+
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("failed to read line");
+
+    let first_number: i32 = first_number.trim().parse().expect("please type first number");
+
+    println!("what function do you want to perform");
+
+    let mut opperation: String = String::new();
+
+     io::stdin()
+        .read_line(&mut opperation)
+        .expect("failed to read line");
+
+    println!("what is your second number?");
+
+    let mut second_number:String = String::new();
+    
+    io::stdin()
+        .read_line(&mut second_number)
+        .expect("failed to read line");
+
+    let second_number: i32 = second_number.trim().parse().expect("please type second number");
+    if opperation.trim() == "+"{
+        let output: i32 = first_number + second_number;
+        println!("{first_number} + {second_number} = {output}");
+    }
+        else if opperation.trim() == "-"{
+        let output: i32 = first_number - second_number;
+        println!("{first_number} - {second_number} = {output}");
+    }
+        else if opperation.trim() == "*"{
+        let output: i32 = first_number * second_number;
+        println!("{first_number} * {second_number} = {output}");
+    }
+        else if opperation.trim() == "/"{
+        let greatestcommondivisor: i32 = gcd(first_number, second_number);
+        if {second_number/greatestcommondivisor} > 1 {
+        let output = {first_number/greatestcommondivisor}.to_string() + "/" + &{second_number/greatestcommondivisor}.to_string();
+        println!("{first_number} / {second_number} = {output}");
+        }
+        else if {second_number/greatestcommondivisor} == 1 {
+        let output: i32 = {first_number/greatestcommondivisor};
+        println!("{first_number} / {second_number} = {output}");
+        }
+        else if second_number == 0 {
+        println!("divide by zero error")
+        }
+        }
+}
